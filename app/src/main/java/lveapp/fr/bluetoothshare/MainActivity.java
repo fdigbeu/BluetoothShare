124c9792a9ab0acd58391fdcf4d697f479561b48
evangile2vie <evangile2vie@gmail.com>
2017-07-14 13:31:46 +0200

    first commit

---
+++ app/src/main/java/lveapp/fr/bluetoothshare/MainActivity.java
@@ -0,0 +1,13 @@
+package lveapp.fr.bluetoothshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
