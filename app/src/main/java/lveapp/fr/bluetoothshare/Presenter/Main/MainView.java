e81cc913e8ac3d548131ae5210ffa7312bbcabe8
evangile2vie <evangile2vie@gmail.com>
2017-07-14 14:24:48 +0200

    Modify file by implementing interface

--- app/src/main/java/lveapp/fr/bluetoothshare/Presenter/Main/MainView.java
+++ app/src/main/java/lveapp/fr/bluetoothshare/Presenter/Main/MainView.java
@@ -1,8 +1,18 @@
 package lveapp.fr.bluetoothshare.Presenter.Main;
 
+import android.widget.TextView;
+
 /**
  * Created by Maranatha on 14/07/2017.
  */
 
 public class MainView {
+    public interface IMainActivity{
+        public void showFieldError(TextView textView);
+        public void showToastMessage(String message);
+    }
+
+    public interface IMainPresenter{
+
+    }
 }
