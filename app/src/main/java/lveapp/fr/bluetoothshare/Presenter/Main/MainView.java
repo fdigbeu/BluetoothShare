c319b92d8945482d5fca6309c2b67e8448383b0c
evangile2vie <evangile2vie@gmail.com>
2017-07-14 14:11:40 +0200

    Reorganization of the project structure by using MVP pattern

---
+++ app/src/main/java/lveapp/fr/bluetoothshare/Presenter/Main/MainView.java
@@ -0,0 +1,8 @@
+package lveapp.fr.bluetoothshare.Presenter.Main;
+
+/**
+ * Created by Maranatha on 14/07/2017.
+ */
+
+public class MainView {
+}
--- app/src/main/java/lveapp/fr/bluetoothshare/View/Activities/MainActivity.java
+++ app/src/main/java/lveapp/fr/bluetoothshare/View/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package lveapp.fr.bluetoothshare;
+package lveapp.fr.bluetoothshare.View.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import lveapp.fr.bluetoothshare.R;
 
 public class MainActivity extends AppCompatActivity {
 
