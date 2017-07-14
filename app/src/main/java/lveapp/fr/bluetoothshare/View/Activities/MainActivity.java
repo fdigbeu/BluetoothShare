40a342ab189e65a4b7604e0a14bd6769728de0e5
evangile2vie <evangile2vie@gmail.com>
2017-07-14 17:40:25 +0200

    Implement of interface

--- app/src/main/java/lveapp/fr/bluetoothshare/View/Activities/MainActivity.java
+++ app/src/main/java/lveapp/fr/bluetoothshare/View/Activities/MainActivity.java
@@ -2,14 +2,26 @@ package lveapp.fr.bluetoothshare.View.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import lveapp.fr.bluetoothshare.Presenter.Main.MainView;
 import lveapp.fr.bluetoothshare.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainView.IMainActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void showFieldError(TextView textView) {
+
+    }
+
+    @Override
+    public void showToastMessage(String message) {
+
     }
 }
