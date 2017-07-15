d4894527669d0d79b01e04266d9e4ffcc5e0cbba
evangile2vie <evangile2vie@gmail.com>
2017-07-15 08:16:11 +0200

    Implementing methods and Adding new method

--- app/src/main/java/lveapp/fr/bluetoothshare/View/Activities/MainActivity.java
+++ app/src/main/java/lveapp/fr/bluetoothshare/View/Activities/MainActivity.java
@@ -2,26 +2,52 @@ package lveapp.fr.bluetoothshare.View.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import lveapp.fr.bluetoothshare.Presenter.Main.MainPresenter;
 import lveapp.fr.bluetoothshare.Presenter.Main.MainView;
 import lveapp.fr.bluetoothshare.R;
 
-public class MainActivity extends AppCompatActivity implements MainView.IMainActivity {
+public class MainActivity extends AppCompatActivity implements MainView.IMainActivity, View.OnClickListener {
+    // Ref widgets
+    private EditText editMessage;
+    private Button buttonShare;
+    // Ref presenter
+    private MainPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //--
+        presenter = new MainPresenter(this);
+        presenter.loadMainData(MainActivity.this);
     }
 
     @Override
-    public void showFieldError(TextView textView) {
+    public void init() {
+        editMessage = (EditText)findViewById(R.id.editMessage);
+        buttonShare = (Button)findViewById(R.id.buttonShare);
+        buttonShare.setOnClickListener(this);
+    }
 
+    @Override
+    public void showFieldError() {
+        editMessage.setError(getResources().getString(R.string.txt_field_require));
     }
 
     @Override
     public void showToastMessage(String message) {
+        Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+    }
 
+    @Override
+    public void onClick(View view) {
+        presenter.setDataEntry(editMessage.getText().toString().trim());
+        presenter.retrieveUserAction(view);
     }
 }
